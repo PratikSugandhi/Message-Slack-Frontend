0a0d16132130e75cd299d5518c4e27a21b40a464
Pratik Sugandhi <pratiksugandhi4@gmail.com>
2026-03-01 12:02:44 +0530

    Added Not Found page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import { SigninCard } from './components/organisms/Auth/SigninCard';
 import { SignupCard } from './components/organisms/Auth/SignupCard';
 import { Auth } from '@/pages/Auth/Auth';
 import { Route, Routes } from 'react-router-dom';
+import { Notfound } from '@/pages/NotFound/NotFound';
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -13,6 +15,8 @@ function App() {
     <Routes>
       <Route path="/auth/signup" element={<Auth><SignupCard /></Auth>} />
       <Route path="/auth/signin" element={<Auth><SigninCard /></Auth>} />
+
+      <Route path="/*" element={<Notfound />} />
     </Routes>
     
   )
