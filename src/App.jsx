ea927001c79f3f0918cf7cd3dca950e8dc23e86b
Pratik Sugandhi <pratiksugandhi4@gmail.com>
2026-03-01 11:14:11 +0530

    Added basic signup card for auth page

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,15 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-
-
+import { Auth } from './pages/Auth/Auth';
+import { Route, Routes } from 'react-router-dom';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <>Hello</>
-      
-    </>
+    <Routes>
+      <Route path="/auth" element={<Auth />} />
+    </Routes>
     
   )
 }
