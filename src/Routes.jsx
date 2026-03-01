1d3535f868f7f6f7c74efd272c949d479c6ab64b
Pratik Sugandhi <pratiksugandhi4@gmail.com>
2026-03-02 01:13:11 +0530

    Added the user button

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -4,13 +4,15 @@ import { SignupContainer } from '@/components/organisms/Auth/SignupContainer';
 import { Auth } from '@/pages/Auth/Auth';
 import { Notfound } from '@/pages/Notfound/Notfound';
 import { ProtectedRoute } from './components/molecules/ProtectedRoute/ProtectedRoute';
+import { Home } from './pages/Home/Home';
+
 
 export const AppRoutes = () => {
     return (
         <Routes>
           <Route path="/auth/signup" element={<Auth><SignupContainer /></Auth>} />
           <Route path="/auth/signin" element={<Auth><SigninContainer /></Auth>} />
-          <Route path="/home" element={<ProtectedRoute><Auth><h1>Home</h1></Auth></ProtectedRoute>} />
+          <Route path="/home" element={<ProtectedRoute><Auth><Home /></Auth></ProtectedRoute>} />
           <Route path="/*" element={<Notfound />} />
         </Routes>
     );
---
+++ src/components/atoms/UserButton/UserButton.jsx
@@ -0,0 +1,31 @@
+import { LogOutIcon, SettingsIcon } from 'lucide-react';
+import { useAuth } from '@/hooks/context/useAuth';
+
+import { Avatar,AvatarFallback,AvatarImage } from '@/components/ui/avatar';
+import { DropdownMenu,DropdownMenuContent,DropdownMenuItem,DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
+export const UserButton = () => {
+
+    const { auth } = useAuth();
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger className='outline-none relative'>
+                <Avatar className='size-10 hover:opacity-65 transition'>
+                    <AvatarImage src={auth?.user?.avatar} />
+                    <AvatarFallback>{auth?.user?.username[0].toUpperCase()}</AvatarFallback>
+                </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuItem>
+                    <SettingsIcon className='size-4 mr-2 h-10' />
+                    Settings
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                    <LogOutIcon className='size-4 mr-2 h-10' />
+                    Logout
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+    
+};
