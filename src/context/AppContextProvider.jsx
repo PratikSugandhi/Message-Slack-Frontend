ae407470c85fc83623b530f7700aa19c80c3c799
Pratik Sugandhi <pratiksugandhi4@gmail.com>
2026-03-06 01:25:02 +0530

    Added basic open preferences modal inside workspace

--- src/context/AppContextProvider.jsx
+++ src/context/AppContextProvider.jsx
@@ -1,10 +1,10 @@
-
+import { WorkspacePreferencesModalContextProvider } from './WorkspacePreferencesModalContext';
 import combineContext from '@/utils/combineContext';
 import { AuthContextProvider } from './AuthContext';
-
 import { CreateWorkspaceContextProvider } from './CreateWorkspaceContext';
 
 export const AppContextProvider = combineContext(
     AuthContextProvider,
-    CreateWorkspaceContextProvider
+    CreateWorkspaceContextProvider,
+    WorkspacePreferencesModalContextProvider
 ); 
