90ac921227cd56e1d57735f96187242c0bddbbf8
Pratik Sugandhi <pratiksugandhi4@gmail.com>
2026-03-13 12:29:54 +0530

    Added css for quill

--- src/components/atoms/Editor/Editor.jsx
+++ src/components/atoms/Editor/Editor.jsx
@@ -1,4 +1,5 @@
 import Quill from 'quill';
+import 'quill/dist/quill.snow.css'; // ES6
 import { useEffect, useRef, useState } from 'react';
 
 export const Editor = ({
@@ -10,15 +11,13 @@ export const Editor = ({
     defaultValue
 }) => {
     
-    const [text, setText] = useState('');
-    const [isToolbarVisible, setIsToolbarVisible] = useState(false);
+    
 
     const containerRef = useRef(); // reqd to initialize the editor
-    const submitRef = useRef(); 
-    const disabledRef = useRef();
+    
     const defaultValueRef = useRef();
     const quillRef = useRef();
-    const placeholderRef = useRef();
+    
 
     useEffect(() => {
 
@@ -30,7 +29,7 @@ export const Editor = ({
 
         const options = {
             theme: 'snow',
-            placeholder: placeholderRef.current,
+            
             modules: {
                 toolbar: [
                     ['bold', 'italic', 'underline', 'strike'],
@@ -74,10 +73,16 @@ export const Editor = ({
         >
 
             <div
-                className='flex flex-col border border-slate-300 rounded-md overflow-hidden focus-within:shadow-sm focus-within:border-slate-400 bg-white transition focus-within:'
+                className='flex flex-col border border-slate-300 rounded-md overflow-hidden focus-within:shadow-sm focus-within:border-slate-400 bg-white'
             >
-                <div ref={containerRef} />
+                <div className='h-full ql-custom' ref={containerRef} />
             </div>
+
+            <p
+                className='p-2 text-[10px] text-mutes-foreground flex justify-end'
+            >
+                <strong>Shift + return</strong> &nbsp; to add a new line
+            </p>
 
         </div>
     );
