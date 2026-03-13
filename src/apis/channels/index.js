13e52bbd9ac3a405fa8e1f8bc7a43c259d7b8f50
Pratik Sugandhi <pratiksugandhi4@gmail.com>
2026-03-13 12:10:24 +0530

    Added basic quill setup for editor

--- src/apis/channels/index.js
+++ src/apis/channels/index.js
@@ -15,3 +15,16 @@ export const deleteChannelRequest = async ({ channelId, token }) => {
     }
 };
 
+export const getChannelById = async ({ channelId, token }) => {
+    try {
+        const response = await axios.get(`/channels/${channelId}`, {
+            headers: {
+                'x-access-token': token
+            }
+        });
+        return response?.data?.data;
+    } catch(error) {
+        console.log('Error in getChannelByIdRequest', error);
+    }
+};
+
